af1998d50859922dd3ae20bf21649a611d086791
Harold Portocarrero <harold.port@gmail.com>
2018-05-24 00:12:07 -0500

    finalizando app

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
-import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import { 
+    getMetricMetaInfo, 
+    timeToString, 
+    getDailyReminderValue,
+    clearLocalNotification,
+    setLocalNotification 
+} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
@@ -82,7 +88,8 @@ class AddEntry extends Component {
         this.toHome()
         submitEntry({ key, entry })
 
-        //Clear local notifications
+        clearLocalNotification()
+            .then(setLocalNotification)
     }
 
     reset = () => {
